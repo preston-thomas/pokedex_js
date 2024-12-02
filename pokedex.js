36ae04b25105fc1eb47654b5a8fd15a8ed38a1c8
preston-thomas <140858770+preston-thomas@users.noreply.github.com>
2024-12-02 10:46:39 -0500

    A few structural fixes

--- pokedex.js
+++ pokedex.js
@@ -1,12 +1,9 @@
-// https://www.w3schools.com/java/java_try_catch.asp
-// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-
 const readline = require("readline");
-const fetch = require("node-fetch")
+const fetch = require("node-fetch");
 
 // Setup readline to listen on the stdin stream
 const rl = readline.createInterface(process.stdin, process.stdout);
- 
+
 function showMenu() {
     console.log("Pokemon Pokedex Menu: ");
     console.log("1: Search Pokemon");
@@ -17,7 +14,7 @@ function showMenu() {
 
 function prompt(cb) {
     rl.question("Enter your command to search through the Pokedex: ", (response) => {
-        cb(response) // the function will call the callback (CB) with the response/user input as the parameter 
+        cb(response); // The function will call the callback (CB) with the response/user input as the parameter 
     });
 }
 
@@ -26,110 +23,95 @@ async function searchPoke(term) {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${term}`);
         if (!response.ok) {
             throw new Error(`Pokemon does not exist.`);
-    }
+        }
         const pokeJSON = await response.json();
-        printPoke(pokeJSON)
-    }
-    catch (err) {
-        console.error("An error has occured while fetching the Pokemon: ", err);
-    }
-    finally {
-        run(); // call run to reprompt users 
+        printPoke(pokeJSON);
+    } catch (err) {
+        console.error("An error has occurred while fetching the Pokemon: ", err);
+    } finally {
+        run(); // Call run to reprompt users 
     }
 }
 
-// modeling structure after searchPoke
 async function searchMove(term) {
     try {
         const response = await fetch(`https://pokeapi.co/api/v2/move/${term}`);
         if (!response.ok) {
             throw new Error(`Move does not exist.`);
-    }
-        const moveJSON = await response.json();
-        for (move in moveJSON) {
-            printMove(moveJSON);
         }
-    }
-    catch (err) {
+        const moveJSON = await response.json();
+        printMove(moveJSON);
+    } catch (err) {
         console.error("An error has occurred while fetching the Move: ", err);
-    }
-    finally {
-        run(); 
+    } finally {
+        run();
     }
 }
 
 async function searchItem(term) {
     try {
-        const response = await fetch('https://pokeapi.co/api/v2/item/${term}');
+        const response = await fetch(`https://pokeapi.co/api/v2/item/${term}`);
         if (!response.ok) {
             throw new Error(`Item does not exist`);
         }
-        const itemJSON = await response.json()
-        printItem(itemJSON)
-    } 
-    catch (err) {
-        console.error("An error has occured while fetching the Item: ", err);
-    }
-    finally {
+        const itemJSON = await response.json();
+        printItem(itemJSON);
+    } catch (err) {
+        console.error("An error has occurred while fetching the Item: ", err);
+    } finally {
         run();
     }
 }
 
 function printPoke(json) {
     console.log("-- Pokemon Information --");
-    console.log('Pokemon Name: ${json.forms.name}');
-    console.log('Pokemon Height: ${json.game_indices.height}');
-    console.log('Pokemon Weight: ${json.weight}');
-    console.log('Pokemon Base Experience ${json.base_experience}');
-    for (let i = 0; i <json.moves.length; i ++) {
-        console.log('Move: ${json.moves[i]}');
-    };
+    console.log(`Pokemon Name: ${json.name}`);
+    console.log(`Pokemon Height: ${json.height}`);
+    console.log(`Pokemon Weight: ${json.weight}`);
+    console.log(`Pokemon Base Experience: ${json.base_experience}`);
+    for (let i = 0; i < json.moves.length; i++) {
+        console.log(`Move: ${json.moves[i].move.name}`);
+    }
 }
 
 function printItem(json) {
     console.log("-- Item Information --");
-    console.log('Item Name: ${json.name}');
-    console.log('Item Category: ${json.category.name}');
-    console.log('Item Cost: ${json.cost}');
-    for (let i = 0; json.effect_entries.length; i++) {
-        console.log('Effect: ${json.effect_entries[i]}');
-    };
+    console.log(`Item Name: ${json.name}`);
+    console.log(`Item Category: ${json.category.name}`);
+    console.log(`Item Cost: ${json.cost}`);
+    for (let i = 0; i < json.effect_entries.length; i++) {
+        console.log(`Effect: ${json.effect_entries[i].effect}`);
+    }
 }
 
 function printMove(json) {
     console.log("-- Move Information --");
-    console.log('Name: ${json.name}');
-    console.log('Damage Class: ${json.damage_class.name');
-    for (let i = 0; json.effect_entries.length; i++) {
-        console.log('Effect: ${json.effect_entries[i]}');
-    };
-    console.log('Power: ${json.power}');
-    console.log('Accuracy: ${json.accuracy}');
+    console.log(`Name: ${json.name}`);
+    console.log(`Damage Class: ${json.damage_class.name}`);
+    for (let i = 0; i < json.effect_entries.length; i++) {
+        console.log(`Effect: ${json.effect_entries[i].effect}`);
+    }
+    console.log(`Power: ${json.power}`);
+    console.log(`Accuracy: ${json.accuracy}`);
 }
 
 function run() {
     showMenu();
-    rl.question("Please select an option to proceed.", answer => {
-    while (answer) {
+    rl.question("Please select an option to proceed: ", (answer) => {
         if (answer == 1) {
-            prompt(searchPoke()); // prompt will handle the parameter for this
-            break;
-        }
-        if (answer == 2) {
-            prompt(searchItem()); // prompt will handle the parameter for this
-            break;
-        }
-        if (answer == 3) {
-           // prompt(searchMove()); commenting out until we add this function
-           break;
-        }
-        if (answer == 4) {
+            prompt(searchPoke); // Pass the function reference to prompt
+        } else if (answer == 2) {
+            prompt(searchItem); // Pass the function reference to prompt
+        } else if (answer == 3) {
+            prompt(searchMove); // Pass the function reference to prompt
+        } else if (answer == 4) {
             console.log(`Good luck, Pokemon trainer!`);
             rl.close();
-            break;
+        } else {
+            console.log("Invalid choice. Please try again.");
+            run();
         }
-    }
+    });
 }
-)};
 
 run();
